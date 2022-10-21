57755ed64bb02be1410f2cfecf18db477fa17074
Fabian Wagner <wafab112@gmail.com>
2022-10-21 10:58:09 +0200

    float to vec3

---
+++ src/Vector.cpp
@@ -0,0 +1,12 @@
+struct vec3 {
+    float x;
+    float y;
+    float z;
+};
+
+struct vec4 {
+    float x;
+    float y;
+    float z;
+    float w;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+
+// Preinstalled .so / .dll / .dylib
 #include <GL/glew.h>
+// Is built system specifically
 #include "GLFW/glfw3.h"
+
+// For file io
 #include <fstream>
+
+// For creating a string 
 #include <sstream>
 #include <string>
-#include <unistd.h>
+
+#include <cstring>
+
+#include "Vector.cpp"
 
 static void glfw_error_callback(int error, const char* descr) {
     std::cerr << error << ": " << descr << "\n";
@@ -18,36 +28,55 @@ static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int act
 
 /**
  * shader has to be already generated
+ * returns true, iff the compilation was successful
  */
-void load_shader(GLuint shader, std::string file_name) {
-    std::ifstream file;
+bool load_shader(GLuint shader_index, std::string shader_file_name) {
+    std::ifstream shader_file;
 
-    file.open(file_name);
+    shader_file.open(shader_file_name);
 
-    std::stringstream ss;
-    
+    if (shader_file.fail()) {
+        std::cerr << "Shader '" << shader_file_name << "' failed to open.\n";
+        std::cerr << "Error Message: " << std::strerror(errno) << std::endl;
+        return false;
+    }
+
+    std::stringstream shader_stream;
     std::string line;
 
-    if (!file.is_open()) {
-        std::cerr << "File '" << file_name << "' not open." << std::endl;
-        return;
+    while (getline(shader_file, line)) {
+        shader_stream << line << "\n";
     }
 
-    while (getline(file, line)) {
-        ss << line << "\n";
+    std::string puffer = shader_stream.str();
+    const char* shader = puffer.data();
+
+    glShaderSource(shader_index, 1, &shader, NULL);
+    glCompileShader(shader_index);
+
+    int shader_compilation_successful = GL_TRUE;
+    glGetShaderiv(shader_index, GL_COMPILE_STATUS, &shader_compilation_successful);
+
+    if (shader_compilation_successful == GL_FALSE) {
+        GLsizei log_length;
+
+        glGetShaderiv(shader_index, GL_INFO_LOG_LENGTH, &log_length);
+
+        char log[log_length];
+        glGetShaderInfoLog(shader_index, log_length, NULL, log);
+
+        std::cerr << "Shader log for " << shader_index << " ('" << shader_file_name << "'): " << log << std::endl;
+
+        return false;
     }
 
-    std::string full_shader = ss.str();
-    const char * c_shader =  full_shader.data();
-
-    glShaderSource(shader, 1, &c_shader, NULL);
-    glCompileShader(shader);
+    return true;
 }
 
 int main() {
     if (!glfwInit()) {
         std::cout << "glfw init failed\n";
-        return -1;
+        return 1;
     }
 
     glfwSetErrorCallback(glfw_error_callback);
@@ -61,7 +90,7 @@ int main() {
     if (!window) {
         std::cout << "glfw window creation failed\n";
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
     glfwMakeContextCurrent(window);
@@ -70,22 +99,23 @@ int main() {
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
         std::cout << "glew init failed\n";
-        return -1;
+        glfwTerminate();
+        return 1;
     }
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    
-    float points[] = {
-        0.0f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f
+
+    vec3 points[] = {
+        vec3 {-0.5f, 0.5f, 0.0f},
+        vec3 {0.5f, 0.5f, 0.0f},
+        vec3 {0.5f, -0.5f, 0.0f}
     };
 
     GLuint vbo = 0;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(vec3), points, GL_STATIC_DRAW);
 
     GLuint vao = 0;
     glGenVertexArrays(1, &vao);
@@ -97,22 +127,34 @@ int main() {
     // Problem: Durch make run ist cwd in /.../clife/, nicht in /.../clife/build
     //      => shaders Verzeichnis ist falsch in runtime
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    load_shader(vs, "shaders/test_vs.glsl");
-
-    //glShaderSource(vs, 1, &vertex_shader, NULL);
-    //glCompileShader(vs);
+    if (!load_shader(vs, "shaders/test_vs.glsl")) {
+        glfwTerminate();
+        return 1;
+    }
 
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    load_shader(fs, "shaders/test_fs.glsl");
-
-    //glShaderSource(fs, 1, &fragment_shader, NULL);
-    //glCompileShader(fs);
+    if (!load_shader(fs, "shaders/test_fs.glsl")) {
+        glfwTerminate();
+        return 1;
+    }
 
     GLuint shader_program = glCreateProgram();
     glAttachShader(shader_program, fs);
     glAttachShader(shader_program, vs);
     glLinkProgram(shader_program);
 
+    int program_linking_successful = GL_TRUE;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &program_linking_successful);
+
+    if (program_linking_successful == GL_FALSE) {
+        GLsizei log_length;
+        glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length);
+
+        char log[log_length];
+        glGetProgramInfoLog(shader_program, log_length, NULL, log);
+        std::cerr << "Shader log for " << shader_program << ": " << log << std::endl;
+        return 1;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
